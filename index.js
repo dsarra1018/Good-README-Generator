4065bbc6b54eaf813218ce93b2ae5203e1882fb4
Darren Sarra <adriansarra18@gmail.com>
2020-03-29 14:08:07 -0400

    Modularizing the questions and templates.

--- index.js
+++ index.js
@@ -2,54 +2,20 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const axios = require("axios");
+const question = require("./script/questions");
+const temp = require("./script/template");
 
-inquirer
-    .prompt([
-        {
-            type: "input",
-            message: "What's your project's title?",
-            name: "title"
-        },
-        {
-            type: "input",
-            message: "Describe your project.",
-            name: "description"
-        },
-        {
-            type: "input",
-            message: "Instructions on how to install.",
-            name: "installation"
-        },
-        {
-            type: "input",
-            message: "What are the uses of your project?",
-            name: "usage"
-        },
-        {
-            type: "input",
-            message: "What license is your project using?",
-            name: "license"
-        },
-        {
-            type: "input",
-            message: "Who are the contributors?",
-            name: "contributors"
-        },
-        {
-            type: "input",
-            message: "What is your Github username?",
-            name: "username"
-        }
-    ])
-    .then(({username}) => {
-        const queryUrl = `https://api.github.com/users/${username}`;
-        const queryUrl2 = `https://api.github.com/users/${username}/events/public`;
+// prompt the user to 
+const answers = inquirer.prompt(question.question);
+answers.then(({username}) => {
+    const queryUrl = `https://api.github.com/users/${username}`;
+    const queryUrl2 = `https://api.github.com/users/${username}/events/public`;
 
-        // axios.get(queryUrl).then( response =>{
-        //     console.log(response.data);
-        // })
+    // axios.get(queryUrl).then( response =>{
+    //     console.log(response.data);
+    // })
 
-        axios.get(queryUrl2).then(response =>{
-            console.log(response.data[0].payload.commits[0]);
-        })
+    axios.get(queryUrl2).then(response =>{
+        console.log(response.data[0].payload.commits[0]);
     })
+})
---
+++ script/questions.js
@@ -0,0 +1,41 @@
+let question = [
+    {
+        type: "input",
+        message: "What's your project's title?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Describe your project.",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Instructions on how to install the project.",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "What are the uses of your project?",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "What license is your project using?",
+        name: "license"
+    },
+    {
+        type: "input",
+        message: "Who are the contributors?",
+        name: "contributors"
+    },
+    {
+        type: "input",
+        message: "What is your Github username?",
+        name: "username"
+    }
+]
+
+module.exports = {
+    question
+}
