117ba2ea7c76f3d423bb93dd42e70f4621cd14a2
Darren Sarra <adriansarra18@gmail.com>
2020-03-28 20:03:55 -0400

    added question prompt

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
 // initializing all requires
 const fs = require("fs");
 const inquirer = require("inquirer");
+
+inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "What's your project's title?",
+            name: "title"
+        },
+        {
+            type: "input",
+            message: "Describe your project.",
+            name: "description"
+        },
+        {
+            type: "input",
+            message: "Instructions on how to install.",
+            name: "installation"
+        },
+        {
+            type: "input",
+            message: "What are the uses of your project?",
+            name: "usage"
+        },
+        {
+            type: "input",
+            message: "What license is your project using?",
+            name: "license"
+        },
+        {
+            type: "input",
+            message: "Who are the contributors?",
+            name: "contributors"
+        },
+        {
+            type: "input",
+            message: "What is your Github username?",
+            name: "username"
+        }
+    ]);
