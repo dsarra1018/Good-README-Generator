1d38763bec656a69db6bcae9904a456eeb9b3e40
Darren Sarra <adriansarra18@gmail.com>
2020-03-29 14:20:15 -0400

    Created a template for README.

--- script/template.js
+++ script/template.js
@@ -1,1 +1,33 @@
-const template = (title)
+const template = (title, description, installation, usage, license, contributors, username) => {
+    `# ${title}
+
+    ## Description
+    
+    ${description}
+    
+    ## Table of Contents
+    
+    [Installation](#Installation) | [Usage](#Usage) | [Credits](#Credits) | [License](#License)
+    
+
+    ## Installation
+    
+    ${installation}
+    
+    ## Usage
+    
+    ${usage}
+    
+    
+    ## Credits
+    
+    ${contributors}
+    
+    ## License
+    
+    ${license}
+    
+    ## Badges
+    
+    `
+}
