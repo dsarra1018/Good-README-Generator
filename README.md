659f3409dbd58ea77142089beae392c13f6d1e76
Darren Sarra <adriansarra18@gmail.com>
2020-03-29 22:08:15 -0400

    updated index.js into an async function

--- README.md
+++ README.md
@@ -1,34 +1,36 @@
-# 
+# Good-README-Generator
 
 ## Description
-    
-
-    
+            
+An application that generates a README.md
+            
 ## Table of Contents
-    
-[Installation](#Installation) | [Usage](#Usage) | [Credits](#Credits) | [License](#License)
-    
-
+            
+[Installation](#Installation) | [Usage](#Usage) | [Contributing](#Contributing) | [License](#License)
+            
+        
 ## Installation
-    
-
-    
+            
+node index.js
+            
 ## Usage
-    
-
-    
-    
-## Credits
-    
-
-    
+            
+Initializes a README for a project
+            
+            
+## Contributing
+            
+n/a
+            
 ## License
-    
-
-
+            
+MIT
+        
+## Test
+        
 ![](undefined&s=200)
-    
-## Badges
-    
+            
+## Questions
+            
 [![](https://img.shields.io/badge/gitHub-dsarra1018-blue?style=plastic)](https://www.github.com/dsarra1018) | 
-[![](https://img.shields.io/badge/email-undefined-purple?style=plastic)](mailto:undefined)
+[![](https://img.shields.io/badge/email-adriansarra18@gmail.com-purple?style=plastic)](mailto:adriansarra18@gmail.com)
--- index.js
+++ index.js
@@ -4,72 +4,76 @@ const inquirer = require("inquirer");
 const axios = require("axios");
 const question = require("./script/questions");
 
-// prompt the user to enter information
-const answers = inquirer.prompt(question.question);
+async function generateREADME() {
+    try{
+        // prompt the user to enter information
+        const answers = await inquirer.prompt(question.question);
 
-// axios API call to github
-answers.then(({username}) =>{
-    const queryUrl = `https://api.github.com/users/${username}`;
-    const queryUrl2 = `https://api.github.com/users/${username}/events/public`;
+        // getting user avatar
+        // let avatar = await axios.get(`https://api.github.com/users/${answers.username}`);
+        // answers.userAvatar = avatar.avatar_url;
+        // console.log(answers.userAvatar)
 
-    // getting user avatar
-    axios.get(queryUrl).then(response => {
-        let avatar = response.data.avatar_url;
-        response.userAvatar = avatar;
-    })
+        // getting user email
+        let email = await axios.get(`https://api.github.com/users/${answers.username}/events/public`);
+        answers.userMail = email.data[0].payload.commits[0].author.email;
+        console.log(answers.userMail);
 
-    // getting user email
-    axios.get(queryUrl2).then(response => {
-        let userMail = response.data[0].payload.commits[0].author.email;
-        response.email = userMail;
-        console.log(response.email);
-    })
-
-})
-answers.then((response) =>{
-    let readme = `# ${response.title}
+        // template literal
+        let readme = `# ${answers.title}
 
 ## Description
-    
-${response.description}
-    
+            
+${answers.description}
+            
 ## Table of Contents
-    
+            
 [Installation](#Installation) | [Usage](#Usage) | [Contributing](#Contributing) | [License](#License)
-    
-
+            
+        
 ## Installation
-    
-${response.installation}
-    
+            
+${answers.installation}
+            
 ## Usage
-    
-${response.usage}
-    
-    
+            
+${answers.usage}
+            
+            
 ## Contributing
-    
-${response.contributors}
-    
+            
+${answers.contributors}
+            
 ## License
-    
-${response.license}
-
+            
+${answers.license}
+        
 ## Test
-
-![](${response.userAvatar}&s=200)
-    
+        
+![](${answers.userAvatar}&s=200)
+            
 ## Questions
+            
+[![](https://img.shields.io/badge/gitHub-${answers.username}-blue?style=plastic)](https://www.github.com/${answers.username}) | 
+[![](https://img.shields.io/badge/email-${answers.userMail}-purple?style=plastic)](mailto:${answers.userMail})`
     
-[![](https://img.shields.io/badge/gitHub-${response.username}-blue?style=plastic)](https://www.github.com/${response.username}) | 
-[![](https://img.shields.io/badge/email-${response.email}-purple?style=plastic)](mailto:${response.email})`
+        // writing a file
+        fs.writeFile("README.md", readme, "utf8", function(err){
+            if (err){
+                console.log(err);
+            }
+            else {
+                console.log("README successfully generated.");
+            }
+        })
 
-    fs.writeFile("README.md", readme, "utf8", function(err){
-        if (err){
-            console.log(err);
-        }
-        else {
-            console.log("README successfully generated.");
-        }
-    })
-})
+        
+
+    }catch(err){
+        console.log(err);
+    }
+}
+
+
+generateREADME();
+
